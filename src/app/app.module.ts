93cdd0160d1c9185e94355122bcad5bb4909243c
Radmila <radmila@prozess.no>
2017-09-22 15:25:29 +0200

    Test a routed component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,21 +11,23 @@ import { DependencyAsyncComponent } from './dependency-async/dependency-async.co
 import {DashboardHeroComponent} from './input-and-output/dashboard-hero/dashboard-hero.component';
 import {InputAndOutputComponent} from './input-and-output/input-and-output.component';
 import {DashboardHeroTestHostComponent} from './input-and-output/dashboard-hero-test-host-component/dashboard-hero-test-host-com.component';
+import {RouterComponentComponent} from "./router-component/router-component.component";
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SimpleComponent,
-    DependencyComponent,
-    DependencyAsyncComponent,
-    InputAndOutputComponent,
-    DashboardHeroComponent,
-    DashboardHeroTestHostComponent
+      AppComponent,
+      SimpleComponent,
+      DependencyComponent,
+      DependencyAsyncComponent,
+      InputAndOutputComponent,
+      DashboardHeroComponent,
+      DashboardHeroTestHostComponent,
+      RouterComponentComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
+      BrowserModule,
+      FormsModule,
+      HttpModule
   ],
   providers: [
       UsersService
