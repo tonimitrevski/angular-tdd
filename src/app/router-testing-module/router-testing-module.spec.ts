c0a73521e7ae9e050e48414b8eef03be738beb4e
Radmila <radmila@prozess.no>
2017-09-29 15:30:38 +0200

    Testing routes with RouterTestingModule

---
+++ src/app/router-testing-module/router-testing-module.spec.ts
@@ -0,0 +1,57 @@
+/* tslint:disable:no-unused-variable */
+import {Location} from '@angular/common';
+import {TestBed, fakeAsync, tick} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {Router} from '@angular/router';
+
+import {
+    HomeComponent,
+    SearchComponent,
+    routes, AppComponentTest
+} from './router-testing-module';
+
+describe('Router Testing Module', () => {
+
+    let location: Location;
+    let router: Router;
+    let fixture;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [    RouterTestingModule.withRoutes(routes)],
+            declarations: [
+                HomeComponent,
+                SearchComponent,
+                AppComponentTest
+            ]
+        });
+
+        router = TestBed.get(Router);
+        location = TestBed.get(Location);
+
+        fixture = TestBed.createComponent(AppComponentTest);
+        router.initialNavigation();
+    });
+
+    it('fakeAsync works', fakeAsync(() => {
+        const promise = new Promise((resolve) => {
+            setTimeout(resolve, 10);
+        });
+        let done = false;
+        promise.then(() => done = true);
+        tick(50);
+        expect(done).toBeTruthy();
+    }));
+
+    it('navigate to "home" takes you to /home', () => {
+        router.navigate([' ']).then(() => {
+            expect(location.path()).toBe('/home');
+        });
+    });
+    //
+    it('navigate to "search" takes you to /search', () => {
+        router.navigate(['/search']).then(() => {
+            expect(location.path()).toBe('/search');
+        });
+    });
+});
